58c98a5bea50f24202cac8f3df28abaaf9cfa47a
Duy <duy270201@gmail.com>
2023-08-23 17:33:21 +0700

    deployment

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  Subscribe,
+  SpecialOffer,
+}
